6ffe58bf4cf60d806d17515cc0614bddd635e4e2
Mayke Rezende <maykemrezende@gmail.com>
2018-01-16 19:50:16 -0200

    Criação de mapsactivity, intent pra maps activity e registro do projeto pra conseguir a api key pra uso do maps
    1a tarefa da aula 3 do terceiro curso de android.

--- app/src/main/java/com/example/mayke/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/example/mayke/agenda/ListaAlunosActivity.java
@@ -164,6 +164,10 @@ public class ListaAlunosActivity extends AppCompatActivity {
                 Intent vaiParaProvas = new Intent(this, ProvasActivity.class);
                 startActivity(vaiParaProvas);
                 break;
+            case R.id.menu_mapa:
+                Intent vaiParaMapa = new Intent(this, MapsActivity.class);
+                startActivity(vaiParaMapa);
+                break;
         }
 
         return super.onOptionsItemSelected(item);
