2dd5952825662259d7933147e9d3361f8cef3d76
Mayke Rezende <maykemrezende@gmail.com>
2018-01-16 20:00:35 -0200

    Criação de mapa activity do zero, em vez da disponibilizada pela google
    2a tarefa da aula 3 do terceiro curso de android.

--- app/src/main/java/com/example/mayke/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/example/mayke/agenda/ListaAlunosActivity.java
@@ -165,7 +165,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
                 startActivity(vaiParaProvas);
                 break;
             case R.id.menu_mapa:
-                Intent vaiParaMapa = new Intent(this, MapsActivity.class);
+                Intent vaiParaMapa = new Intent(this, MapaActivity.class);
                 startActivity(vaiParaMapa);
                 break;
         }
