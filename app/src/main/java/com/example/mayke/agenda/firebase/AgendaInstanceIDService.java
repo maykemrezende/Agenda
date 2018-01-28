197d650619a2d5a9f12dd8349a664ea4bc26a769
Mayke Rezende <maykemrezende@gmail.com>
2018-01-27 23:10:08 -0200

    Commit para correção de erro d epush notification do firebase, cujo refreshed token não está sendo pego

---
+++ app/src/main/java/com/example/mayke/agenda/firebase/AgendaInstanceIDService.java
@@ -0,0 +1,27 @@
+package com.example.mayke.agenda.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by mayke on 26/01/2018.
+ */
+
+public class AgendaInstanceIDService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("token firebase", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        enviaTokenParaServidor(refreshedToken);
+    }
+
+    private void enviaTokenParaServidor(String refreshedToken) {
+    }
+}
