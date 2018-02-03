ade638f97c174b7cdf0603079ec0867afe6766b1
Mayke Rezende <maykemrezende@gmail.com>
2018-02-03 14:13:19 -0200

    Persistindo os dados no celular, do aluno que é gravado pela aplicação web.
    Tarefas 1 da aula 3 do 2o curso de android com web service

--- app/src/main/java/com/example/mayke/agenda/firebase/AgendaMessagingService.java
+++ app/src/main/java/com/example/mayke/agenda/firebase/AgendaMessagingService.java
@@ -2,9 +2,13 @@ package com.example.mayke.agenda.firebase;
 
 import android.util.Log;
 
+import com.example.mayke.agenda.dao.AlunoDAO;
+import com.example.mayke.agenda.dto.AlunoSync;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -19,5 +23,23 @@ public class AgendaMessagingService extends FirebaseMessagingService {
 
         Map<String, String> mensagemDoServidor = remoteMessage.getData();
         Log.i("mensagem recebida", String.valueOf(mensagemDoServidor));
+
+        converteParaAluno(mensagemDoServidor);
     }
+
+    private void converteParaAluno(Map<String, String> mensagemDoServidor) {
+        String chaveDeAcesso = "alunoSync";
+        if(mensagemDoServidor.containsKey(chaveDeAcesso)){
+            String json = mensagemDoServidor.get(chaveDeAcesso);
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                AlunoSync alunoSync = mapper.readValue(json, AlunoSync.class);
+                AlunoDAO alunoDAO = new AlunoDAO(this);
+                alunoDAO.SincronizaAlunosDoServidor(alunoSync.getAlunos());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
